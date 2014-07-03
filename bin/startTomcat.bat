127722d9759fd53ab4c0a2394ee6bf544df4cc5c
xufucheng <xfcjscn@gmail.com>
2014-07-03 13:26:39 +0800

    add deploy script

---
+++ bin/startTomcat.bat
@@ -0,0 +1,2 @@
+cd ..
+mvn -P runtime.appsvr,runtime.appsvr.tomcat,runtime.appsvr.new,runtime.appsvr.nostop,deploy.app install %1
---
+++ bin/startWildfly.bat
@@ -0,0 +1,2 @@
+cd ..
+mvn -P runtime.appsvr,runtime.appsvr.wildfly,runtime.appsvr.local,runtime.appsvr.nostop,deploy.app install %1
